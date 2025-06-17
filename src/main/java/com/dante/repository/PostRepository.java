201f97a3b3be99810ec9009b1cf65798fbee05f0
dante <heywheredante@gmail.com>
2025-06-18 00:28:46 +0900

    feat: repository 추가

---
+++ src/main/java/com/dante/repository/PostRepository.java
@@ -0,0 +1,15 @@
+package com.dante.repository;
+
+import com.dante.model.Post;
+import com.dante.model.Subreddit;
+import com.dante.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllBySubreddit(Subreddit subreddit);
+    List<Post> findByUser(User user);
+}
---
+++ src/main/java/com/dante/repository/VoteRepository.java
@@ -0,0 +1,14 @@
+package com.dante.repository;
+
+import com.dante.model.Post;
+import com.dante.model.User;
+import com.dante.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
+}
